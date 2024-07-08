c5797d394bed5c2c7d05d3da9fec7b27feef4530
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-07-08 09:06:25 +0000

    Updating the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { Routes, Route } from "react-router-dom"
 import { BusinessDirectory, BusinessProfile, CommunityForums, EventCalendar, Home, UserDashboard } from "./pages"
+import { Footer, Header } from "./components"
 
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/directory" element={<BusinessDirectory />} />
-      <Route path="/business/:id" element={<BusinessProfile />} />
-      <Route path="/events" element={<EventCalendar />} />
-      <Route path="/forums" element={<CommunityForums />} />
-      <Route path="/dashboard" element={<UserDashboard />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/directory" element={<BusinessDirectory />} />
+          <Route path="/business/:id" element={<BusinessProfile />} />
+          <Route path="/events" element={<EventCalendar />} />
+          <Route path="/forums" element={<CommunityForums />} />
+          <Route path="/dashboard" element={<UserDashboard />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
