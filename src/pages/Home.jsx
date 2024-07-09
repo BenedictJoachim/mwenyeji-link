cdf17f0c6e19b653755074ea897f9360bffe715d
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-07-09 17:43:30 +0000

    Fixing the home page images for small screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { compass, map_compass, town_map } from "../assets/images";
 const Home = () => {
   return (
     <div className='relative'>
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center z-10">
       <h1 className="text-5xl text-blue-600 font-montserrat font-bold mb-8 ml-8">You won't believe! <br /> what this location has to offer, <br /> just talk to Mwenyeji.</h1>
       <p className="text-lg text-gray-700 mb-8 mx-8">Discover local businesses and events in your community.</p>
       <div className="space-x-4">
@@ -19,12 +19,12 @@ const Home = () => {
       <img 
         src={compass} 
         alt="compass"
-        className='absolute top-16 left-0 w-1/5' 
+        className='hidden md:block absolute top-16 left-0 z-6 w-1/5' 
       />
       <img 
         src={town_map} 
         alt="town map"
-        className='absolute bottom-16 right-0 w-2/5'  
+        className='absolute bottom-[50%] md:bottom-16 right-0 z-6 w-2/5'  
       />
     </div>
   );
